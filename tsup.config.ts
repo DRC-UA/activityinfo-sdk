f81fb7b07e0a62a6688a8045e41ff09a9255ebee
Alexandre Annic <alexandre.annic1@gmail.com>
2025-02-17 09:04:12 -0500

    fix: tsup declaration

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from 'tsup'
 
 export default defineConfig({
   entry: {
-    main: 'src/index.ts',
+    index: 'src/index.ts',
     interface: 'src/interface/index.ts',
   },
   format: ['esm', 'cjs'],
