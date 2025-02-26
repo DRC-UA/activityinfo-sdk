a3e12c98772acbdd00447511b302ebec5807cfd8
Alexandre Annic <alexandre.annic1@gmail.com>
2025-02-26 11:45:30 -0500

    feat(schema): Build AiLocationMap

--- src/schema-builder/AiBuilder.spec.ts
+++ src/schema-builder/AiBuilder.spec.ts
@@ -1,5 +1,5 @@
-﻿import {AiBuilder} from './AiBuilder'
-import {env} from '../utils/Env'
+﻿import { AiBuilder } from './AiBuilder'
+import { env } from '../utils/Env'
 
 describe('AiBuilder', () => {
   it('Run 2024', async () => {
@@ -89,8 +89,8 @@ describe('AiBuilder', () => {
         },
         // Raion: {skipChoices: true},
         // Hromada: {skipChoices: true},
-        Settlement: {skipChoices: true},
-        'Collective Site': {skipChoices: true},
+        Settlement: { skipChoices: true },
+        'Collective Site': { skipChoices: true },
       },
     })
     //   await builder24.generateInterface({
@@ -139,7 +139,57 @@ describe('AiBuilder', () => {
     //   })
   })
 
-  it('Run 2025', async () => {
+  it.only('Build 2025 location map', async () => {
+    await new AiBuilder({
+      activityInfoToken: env.ACTIVITY_INFO_API_TOKEN,
+      outDir: env.ROOT_DIR + '/ua/2025/',
+    }).generateSchema({
+      formId: 'c95ky7klr95z6ia3v',
+      fileName: 'LocationMap',
+      questionSettings: {
+        Oblast: {
+          selectColumnByLabels: ['Admin1 Pcode'],
+        }, 
+        Raion: {
+          selectColumnByLabels: ['Admin2 Pcode'],
+        }, 
+        Hromada: {
+          selectColumnByLabels: ['Admin3 Pcode'],
+        }, 
+        Settlement: {
+          selectColumnByLabels: ['Admin4 Pcode'],
+        },
+        'Collective Site': { skip: true },
+        'Date of completion/ distribution (YYYY-MM-DD)': { skip: true },
+        Comment: { skip: true },
+        'Outside HNRP Scope sub-categories': { skip: true },
+        'Reporting Organization': { skip: true },
+        'Implementing Partner': { skip: true },
+        'Plan/Project Code': { skip: true },
+        'Indicators - SNFI': { skip: true },
+        'Distribution through Common Pipeline': { skip: true },
+        Theme: { skip: true },
+        'Collective Sites': { skip: true },
+        'Reporting Month': { skip: true },
+        'Activity Start month': { skip: true },
+        'Activity End month': { skip: true },
+        'Population Group': { skip: true },
+        'Non-individuals Reached': { skip: true },
+        'Total Individuals Reached': { skip: true },
+        'Girls (0-17)': { skip: true },
+        'Boys (0-17)': { skip: true },
+        'Adult Women (18-59)': { skip: true },
+        'Adult Men (18-59)': { skip: true },
+        'Older Women (60+)': { skip: true },
+        'Older Men (60+)': { skip: true },
+        'People with disability': { skip: true },
+        'HNRP Scope': { skip: true },
+        'Outside HNRP Scope sub-category': { skip: true },
+      },
+    })
+  })
+
+  it('Build 2025 form', async () => {
     const builder25 = new AiBuilder({
       activityInfoToken: env.ACTIVITY_INFO_API_TOKEN,
       outDir: env.ROOT_DIR + '/ua/2025/',
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,1 +1,2 @@
 export * as schema from './namespace'
+export {aiLocationMap} from './ua/2025/AiLocationMap'
